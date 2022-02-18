d071ff6e129875de2453461ac12e0b1710d5fcd3
Manav Misra <manavm@visionwebsoft.com>
2022-02-18 07:03:44 -0600

    :white_check_mark: Test title

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import renderer from "react-test-renderer";
 import App from "./App";
 
+const title = "🥘 OpinionAte";
+
 describe("My Test Suite", () => {
   it("My Test Case", () => {
     expect(true).toBe(true);
@@ -12,5 +14,10 @@ describe("App", () => {
   test("snapshot renders", () => {
     const component = renderer.create(<App />);
     expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it(`has title of ${title}`, () => {
+    renderer.create(<App />);
+    expect(document.title).toBe("🥘 OpinionAte");
   });
 });
