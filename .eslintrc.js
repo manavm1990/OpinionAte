33d7c8d973c0fff603fd7f29516bd1aae6bcc168
Manav Misra <manavm@visionwebsoft.com>
2022-02-18 21:51:40 -0600

    chore: :wrench: Make 'arrow-body-style' a warning
    arrow-body-style": "warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   },
   plugins: ["react", "jsx-a11y", "jest"],
   rules: {
+    "arrow-body-style": "warn",
     "capitalized-comments": "off",
     "react/react-in-jsx-scope": "off",
     "no-unused-vars": "warn",
